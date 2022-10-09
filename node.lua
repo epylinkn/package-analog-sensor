2bc670e4d603b6bb01cfe0efeafdd30693e3a4a4
Anthony Bui <anthonybui@gmail.com>
2022-10-08 22:19:00 -0700

    print statements

--- node.lua
+++ node.lua
@@ -13,8 +13,10 @@ util.data_mapper{
 function node.render()
     pp(on)
     if (on > 700) then
+        pp("green")
         gl.clear(0, 1, 0, 1) -- green
     else
+        pp("red")
         gl.clear(1, 0, 0, 1) -- red
     end
 end
