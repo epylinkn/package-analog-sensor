302f4185d0a8a6926daa0f6f9c1349d1be33e917
Anthony Bui <anthonybui@gmail.com>
2022-10-17 21:43:53 -0700

    Please lord

--- node.lua
+++ node.lua
@@ -4,17 +4,40 @@ util.no_globals()
 
 local on = 0
 
+local video_one
+local video_two
+
 util.data_mapper{
     state = function(state)
         on = tonumber(state) -- comes in as string!!!
     end,
 }
 
+util.json_watch("config.json", function(config)
+    if video_one then
+        video_one:dispose()
+    end
+    if video_two then
+        video_two:dispose()
+    end
+
+    video_one = resource.load_video{
+        file = config.video1.asset_name,
+        looped = true,
+    }
+    video_two = resource.load_video{
+        file = config.video2.asset_name,
+        looped = true,
+    }
+end)
+
 function node.render()
     pp(on)
     if on > 700 then
-        gl.clear(0, 1, 0, 1) -- green
+        video_one.draw(0, 0, WIDTH, HEIGHT)
+        -- gl.clear(0, 1, 0, 1) -- green
     else
-        gl.clear(1, 0, 0, 1) -- red
+        video_two.draw(0, 0, WIDTH, HEIGHT)
+        -- gl.clear(1, 0, 0, 1) -- red
     end
 end
