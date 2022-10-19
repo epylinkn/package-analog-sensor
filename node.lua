29e66f5be436bea5050fd5ef5562b13ef648151d
Anthony Bui <anthonybui@gmail.com>
2022-10-19 12:25:23 -0700

    Dumbest operator ever?

--- node.lua
+++ node.lua
@@ -88,7 +88,7 @@ function node.render()
 
     if configuring then
         font:write(120, 320, "CONFIGURATION MODE", 100, 1, 1, 1, 1)
-        font:write(120, 520, ("THRESHOLD: " ... threshold), 100, 1, 1, 1, 1)
-        font:write(120, 620, ("READING: " ... distance), 100, 1, 1, 1, 1)
+        font:write(120, 520, ("THRESHOLD: " .. threshold), 100, 1, 1, 1, 1)
+        font:write(120, 620, ("READING: " .. distance), 100, 1, 1, 1, 1)
     end
 end
